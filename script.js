9797c8e2b8bd511fd9edb4e7fdeaf7b2aa8aeb62
mullerow <maximilian.kohler@posteo.de>
2024-03-26 11:15:04 +0100

    feat: todo app created with vue

--- script.js
+++ script.js
@@ -1,7 +1,76 @@
 Vue.createApp({
   data() {
     return {
-      person: "Doan Joe",
+      todos: [
+        {
+          id: 1,
+          description: "erstmal Hallo",
+          done: false,
+        },
+
+        {
+          id: 2,
+          description: "Wünsch einen guten Morgen!🌤️",
+          done: true,
+        },
+        {
+          id: 3,
+          description: "dann Tschüss!!!",
+          done: true,
+        },
+      ],
+      newTodo: "",
+      filterModus: "allTodos",
+      filteredTodos: [],
     };
+  },
+  methods: {
+    addTodo() {
+      const duplicate = this.todos.some(
+        (todo) =>
+          todo &&
+          todo.description &&
+          todo.description.toLowerCase() === this.newTodo.toLowerCase()
+      );
+      if (this.newTodo === "") {
+        return;
+      } else if (duplicate) {
+        return;
+      } else {
+        this.todos.push({
+          id: Math.random().toString().substr(2),
+          description: this.newTodo,
+          done: false,
+        });
+      }
+    },
+    changeFilterMode(event) {
+      this.filterModus = event.target.id;
+      if (this.filterModus === "allTodos") {
+        return (this.filteredTodos = this.todos);
+      }
+      if (this.filterModus === "openTodos") {
+        this.filteredTodos = this.todos.filter((todo) => {
+          return todo.done === false;
+        });
+        return this.filteredTodos;
+      }
+      if (this.filterModus === "doneTodos") {
+        this.filteredTodos = this.todos.filter((todo) => {
+          return todo.done === true;
+        });
+        return this.filteredTodos;
+      }
+    },
+    deleteDoneTodos() {
+      for (i = this.todos.length - 1; i >= 0; i--) {
+        if (this.todos[i].done === true) {
+          this.todos.splice(i, 1);
+        }
+      }
+    },
+  },
+  created() {
+    this.filteredTodos = this.todos;
   },
 }).mount("#app");
